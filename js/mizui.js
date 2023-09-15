f28d3032135dc08d6010a478c0e61cceef0090dc
Naman Bishnoi <nmn@n3n.org>
2023-09-15 19:32:34 +0530

    site upd: Fri 15 Sep 2023 19:32:34

--- js/mizui.js
+++ js/mizui.js
@@ -1,36 +1,38 @@
 // Navbar Functions
-const navbar = document.getElementById('navbar');
-const navbarToggle = document.getElementById('navbar-toggle');
+const navbar = document.getElementById("navbar");
+const navbarToggle = document.getElementById("navbar-toggle");
 
-function openMobileNavbar() {
-  navbar.classList.add('opened');
-  navbarToggle.setAttribute('aria-expanded', 'true');
-}
-
-function closeMobileNavbar() {
-  navbar.classList.remove('opened');
-  navbarToggle.setAttribute('aria-expanded', 'false');
-}
-
-navbarToggle.addEventListener('click', () => {
-  if (navbar.classList.contains('opened')) {
-    closeMobileNavbar();
+// Toggle Navbar Menu 
+navbarToggle.addEventListener("click", () => {
+  if (navbar.classList.contains("opened")) {
+    // Close Mobile Navbar
+    navbar.classList.remove("opened");
+    navbarToggle.setAttribute("aria-expanded", "false");
   } else {
-    openMobileNavbar();
+    // Open Mobile Navbar
+    navbar.classList.add("opened");
+    navbarToggle.setAttribute("aria-expanded", "true");
   }
 });
 
+// Add Background to Navbar when scrolling down below certain limit.
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 80) {
+    navbar.classList.add("navbar-bg")
+  } else {
+    navbar.classList.remove("navbar-bg")
+  }
+})
 
 // Alert dismissible
-const alertBar = document.getElementById('alert-closable');
-const alertClose = document.getElementById('alert-close');
+const alertBar = document.getElementById("alert-closable");
+const alertClose = document.getElementById("alert-close");
 
-alertClose.addEventListener('click', () => {
-  if (!alertBar.classList.contains('hide')) {
-    alertBar.classList.add('hide');
+alertClose.addEventListener("click", () => {
+  if (!alertBar.classList.contains("hide")) {
+    alertBar.classList.add("hide");
   }
 });
-
 
 // Search close
 
@@ -38,12 +40,12 @@ let searchBox = document.querySelector(".search-box");
 
 window.onclick = function (e) {
   if (e.target == searchBox) {
-    searchBox.style.display = "none"
+    searchBox.style.display = "none";
   }
-}
+};
 
 window.onkeyup = function (e) {
   if (e.keyCode === 27) {
-    searchBox.style.display="none";
+    searchBox.style.display = "none";
   }
- }
+};
